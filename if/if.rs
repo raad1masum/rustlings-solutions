db50eeaf8c014cc41691dba181dc2a12b95dd12e
raad1masum <piraadwan@gmail.com>
2020-07-26 16:04:02 -0700

    add if1

---
+++ if/if.rs
@@ -0,0 +1,29 @@
+// if1.rs
+
+// I AM NOT DONE
+
+pub fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        return a;
+    } else if b > a {
+        return b;
+    } else {
+        return 0;
+    }
+}
+
+// Don't mind this for now :)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ten_is_bigger_than_eight() {
+        assert_eq!(10, bigger(10, 8));
+    }
+
+    #[test]
+    fn fortytwo_is_bigger_than_thirtytwo() {
+        assert_eq!(42, bigger(32, 42));
+    }
+}
